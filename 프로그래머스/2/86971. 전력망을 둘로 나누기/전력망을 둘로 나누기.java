2952e14d1116c9d9ab6812acac4a24b480970639
doyeon-del <121418701+doyeon-del@users.noreply.github.com>
2026-06-27 06:59:21 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 20.44 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,61 +1,62 @@
 import java.util.*;
+
 class Solution {
-    static int answer; // 최대 차이가 n이니가 
     public int solution(int n, int[][] wires) {
+        int answer = 10000;
         
-        answer = n;
-        
-        // 존재하는 wire 들 중에 어떤 선을 끊을 것인가? 
-        
-        for (int w=0; w<wires.length; w++){ // 끊어줄 와이어 번호
-            
+        for (int cut = 0; cut<wires.length; cut++){ // 어떤 전력망을 끊을 것인지 고르기? 
+                    
             List<List<Integer>> adj = new ArrayList<>();
-            for (int l=0; l<=n; l++){
+            
+            for (int i=0; i<=n; i++){
                 adj.add(new ArrayList<>());
             }
-            
-            // 인접리스트에 wire 추가해주기
-            for (int i=0; i<wires.length; i++){
-                if (i==w) continue; // 이번 기회에 끊는 wire는 패스 
-                
-                int w1 = wires[i][0];
-                int w2 = wires[i][1];
-                
+
+            for (int w = 0; w<wires.length ; w++){
+                if (cut==w) continue; // 해당 부분은 표시안해주기 
+                    
+                int w1 = wires[w][0];
+                int w2 = wires[w][1];
                 adj.get(w1).add(w2);
                 adj.get(w2).add(w1);
-            }
+            } 
+        
+            int c = connect(1,adj,n);
+            int notC = n - c; 
             
-            // 현재의 인접리스트 바탕으로 bfs 탐색 진행
-            boolean[] visited = new boolean[n+1];
-            int diff = bfs(1, adj, visited);
-            answer = Math.min(diff, answer);
-            
+            answer = Math.min(answer, Math.abs(c - notC));
+      
         }
-
+        
         return answer;
     }
     
-    public int bfs(int start, List<List<Integer>> adj, boolean[] visited){
-        int connect = 1; // 최소 1개 이상 연결되니까
-        visited[start] = true;
-        Deque<Integer> queue = new ArrayDeque<>();
-        queue.add(start);
+    public int connect(int start, List<List<Integer>> adj, int n){
         
-        while(!queue.isEmpty()){
-            int current = queue.poll(); 
-            for (int c : adj.get(current)){ // 리스트 돌면서 큐에다가 넣기
-                if (!visited[c]){
-                    visited[c] = true; 
-                    connect++;
-                    queue.add(c); // 다음 값 넣어주고 ㄱ
+        int count = 1;
+        Deque<Integer> q = new ArrayDeque<>();
+        boolean[] visited = new boolean[n+1];
+        
+        q.add(start);
+        visited[start] = true;
+        
+        while(!q.isEmpty()){
+            int temp = q.poll();
+            
+            for (int num=2; num<visited.length; num++){
+                if (adj.get(temp).contains(num) && !visited[num]){
+                    visited[num] = true;
+                    count++;
+                    q.add(num);
                 }
-                
             }
+            
+            
         }
         
-        // 연결된 노드 값 기준으로 차이 구하기 
-        int diff = Math.abs(visited.length - 1 - 2*connect);
-        return diff;
+        return count;
         
+            
+            
     }
 }
