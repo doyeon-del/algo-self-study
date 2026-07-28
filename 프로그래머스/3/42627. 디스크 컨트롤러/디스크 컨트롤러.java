03556129a5a77b8fd8c9cc32d53e1afeeac2bee1
doyeon-del <121418701+doyeon-del@users.noreply.github.com>
2026-07-28 09:40:50 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 4.74 ms, Memory: 88.9 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -12,51 +12,42 @@ class Task {
         this.len = len;
     }
     
+    
 }
 
 class Solution {
     public int solution(int[][] jobs) {
         
-        PriorityQueue<Task> pq = new PriorityQueue<>((a,b) -> (
-            a.len == b.len ?
-            (a.req == b.req ? Integer.compare(a.num, b.num) : Integer.compare(a.req, b.req)) :
-            Integer.compare(a.len, b.len)
-        ));
+        PriorityQueue<Task> pq = new PriorityQueue<>((a,b) -> 
+         a.len == b.len ? 
+        (a.req == b.req ? Integer.compare(a.num, b.num) : Integer.compare(a.req, b.req)) :
+                                                     Integer.compare(a.len, b.len));
         
-        Arrays.sort(jobs, (a,b) -> Integer.compare(a[0], b[0]));
-        
-        int totalTime = 0; // 전체 반환 시간
-        int curTime = 0; // 현재 시간
-        
-        int taskCount = 0;
+        Arrays.sort(jobs, (a,b) -> Integer.compare(a[0], b[0])); // 요청 시간이 빠른 것부터 나열
+        int totalTime = 0;
+        int taskCount = 0; 
         int jobIdx = 0;
+        int curTime = 0;
         
-        while(taskCount < jobs.length){
+        while (taskCount < jobs.length){
             
-            while(jobIdx < jobs.length && jobs[jobIdx][0] <= curTime){
+                        while(jobIdx < jobs.length && jobs[jobIdx][0] <= curTime){
+
                 pq.add(new Task(jobIdx, jobs[jobIdx][0], jobs[jobIdx][1]));
                 jobIdx++;
-                
-                
             }
-            
             
             if (pq.isEmpty()){
                 curTime = jobs[jobIdx][0];
-            } 
-            
-            else { // 현재 작업 처리 
-                    Task cur = pq.poll();
-                    curTime += cur.len;
-                    totalTime += (curTime - cur.req);
-                    taskCount++;
-                    
-                    
+            } else {
+                Task cur = pq.poll();
+                taskCount++;
+                curTime += cur.len;
+                totalTime += (curTime - cur.req);
             }
+            
         }
         
-        
-        
-        return totalTime / jobs.length;
+                return totalTime / taskCount;
     }
 }
