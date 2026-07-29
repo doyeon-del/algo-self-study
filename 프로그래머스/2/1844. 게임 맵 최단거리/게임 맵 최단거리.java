8c2756d36b1ffe357ad442d914d21cf03685f9f1
doyeon-del <121418701+doyeon-del@users.noreply.github.com>
2026-07-29 19:57:09 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 3.95 ms, Memory: 58.1 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,48 +1,64 @@
 import java.util.*;
+
+class Spot {
+    
+    int r;
+    int c;
+    int len;
+    
+    public Spot(int r, int c, int len){
+        this.r = r;
+        this.c = c;
+        this.len = len;
+    }
+    
+}
+
+
 class Solution {
     static int[] dr = {-1,1,0,0};
     static int[] dc = {0,0,-1,1};
-    static int answer;
     
     public int solution(int[][] maps) {
-        answer = -1;
+        
         int n = maps.length;
         int m = maps[0].length;
-        // boolean[][] visited = new visited[n][m];
+        boolean[][] visited = new boolean[n][m];
         
-        return bfs(0,0, n, m, maps);
+        
+        return bfs(maps, visited, n, m);
     }
     
-    public int bfs(int sr, int sc, int er, int ec, int[][] maps){
+    public int bfs(int[][] maps, boolean[][] visited, int r, int c){
         
-        Deque<int[]> q = new ArrayDeque<>();
-        maps[sr][sc] = 0; 
-        q.add(new int[] {sr, sc, 1});
+        visited[0][0] = true;
+        Deque<Spot> q = new ArrayDeque<>();
+        q.add(new Spot(0,0,1));
         
         while(!q.isEmpty()){
-            int[] temp = q.poll();
-            int cr = temp[0];
-            int cc = temp[1];
-            int cl = temp[2];
+            Spot cur = q.poll();
+            int cr = cur.r;
+            int cc = cur.c;
+            int clen = cur.len;
             
-            if (cr == er - 1 && cc == ec - 1){
-
-                return cl; // 바로 종료 
-            }
+            if (cr == r-1 && cc == c-1) return clen;
             
             for (int d=0; d<4; d++){
                 int nr = cr + dr[d];
                 int nc = cc + dc[d];
                 
-                if (nr >=0 && nc>=0 && nr<er && nc<ec && maps[nr][nc]==1){
-                    maps[nr][nc] = 0; // 방문 표시
-                    q.add(new int[] {nr, nc, cl + 1});
+                if (nr >=0 && nc>=0 && nc<c && nr<r){
+                    if(!visited[nr][nc] && maps[nr][nc] == 1){
+                        visited[nr][nc] = true;
+                        q.add(new Spot(nr, nc, clen+1));
+                    }
                 }
                 
             }
+            
         }
         
-        return -1;
         
+        return -1; 
     }
 }
