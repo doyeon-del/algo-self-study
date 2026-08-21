5f29acbe4017183d6ff3b416805fb1faf3947ccb
doyeon-del <121418701+doyeon-del@users.noreply.github.com>
2026-08-21 09:09:58 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 4.15 ms, Memory: 81.7 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -3,51 +3,62 @@ import java.util.*;
 class Task {
     
     int num;
-    int req;
-    int len;
+    int request;
+    int len; // 작업의 소요 시간 
     
-    public Task(int num, int req, int len){
+    public Task(int num, int request, int len){
         this.num = num;
-        this.req = req;
+        this.request = request;
         this.len = len;
     }
-    
-    
 }
+
 
 class Solution {
     public int solution(int[][] jobs) {
+        int answer = 0;
+        // 대기큐를 우선순위 큐로 만들기
         
-        PriorityQueue<Task> pq = new PriorityQueue<>((a,b) -> 
-         a.len == b.len ? 
-        (a.req == b.req ? Integer.compare(a.num, b.num) : Integer.compare(a.req, b.req)) :
-                                                     Integer.compare(a.len, b.len));
+        PriorityQueue<Task> pq = new PriorityQueue<>(
+            // 세가지 우선 순위 조건 바로 넣어주기 
+            (a, b) -> (
+                a.len != b.len ? Integer.compare(a.len, b.len) : (
+                a.request != b.request ? Integer.compare(a.request, b.request) : (
+                Integer.compare(a.num, b.num)))
+            )
+        );
         
-        Arrays.sort(jobs, (a,b) -> Integer.compare(a[0], b[0])); // 요청 시간이 빠른 것부터 나열
-        int totalTime = 0;
-        int taskCount = 0; 
-        int jobIdx = 0;
-        int curTime = 0;
         
-        while (taskCount < jobs.length){
-            
-                        while(jobIdx < jobs.length && jobs[jobIdx][0] <= curTime){
+        // 요청되는 시간 기준 오름차순으로 정렬
+        Arrays.sort(jobs, (a,b) -> Integer.compare(a[0], b[0]));
+        
 
+        int totalTime = 0 ;
+        int curTime = 0; 
+        int jobIdx = 0;
+        int count = 0; 
+        
+        while (count < jobs.length){
+            
+            while (jobIdx < jobs.length && jobs[jobIdx][0] <= curTime){
                 pq.add(new Task(jobIdx, jobs[jobIdx][0], jobs[jobIdx][1]));
                 jobIdx++;
             }
             
             if (pq.isEmpty()){
-                curTime = jobs[jobIdx][0];
+                curTime = jobs[jobIdx][0]; // 현재 작업 요청 시간으로 이동 
             } else {
-                Task cur = pq.poll();
-                taskCount++;
-                curTime += cur.len;
-                totalTime += (curTime - cur.req);
+                Task task = pq.poll();
+                curTime += task.len;
+                totalTime += curTime - task.request;
+                count++;
+                
             }
-            
         }
         
-                return totalTime / taskCount;
+        
+        return totalTime / jobs.length;
+        
+        
     }
 }
